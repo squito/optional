1496f3137199ffa5ef0aa37d34cbfe2753c4e470
Imran Rashid <imran@quantifind.com>
2013-02-25 17:22:48 -0800

    remove everything, point to Sumac repo

--- src/main/scala/optional/types/MultiSelectInput.scala
+++
@@ -1,9 +0,0 @@
-package optional.types
-
-class MultiSelectInput[T](var value: Set[T], val options: Set[T])
-
-object MultiSelectInput {
-  def apply[T](options: T*) = new MultiSelectInput[T](Set(), options.toSet)
-}
-
-
